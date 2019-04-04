068d9794a89b1ce20a4ce0a9d5bb4c320fff9473
szabolcsveer <futuremove@gmail.com>
2019-04-04 13:39:26 +0200

    Two Numbers Operations

---
+++ week-01/day-4/two-numbers.ts
@@ -0,0 +1,8 @@
+let numberOne = 22;
+let numberTwo = 13;
+
+console.log(numberOne + numberTwo);
+console.log(numberOne - numberTwo);
+console.log(numberOne *numberTwo);
+console.log(numberOne / numberTwo);
+console.log(numberOne % numberTwo);
