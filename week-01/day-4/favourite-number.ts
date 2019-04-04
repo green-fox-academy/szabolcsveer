92b3104b2070c225658eaaf44184e0f69bcf9a81
szabolcsveer <futuremove@gmail.com>
2019-04-04 15:19:06 +0200

    printed fav number on screen

--- week-01/day-4/favourite-number.ts
+++ week-01/day-4/favourite-number.ts
@@ -4,3 +4,5 @@ let favoriteNumber: number = 42
 
 // Store your favorite number in a variable (as a number)
 // And print it like this: 'My favorite number is: 8'
+
+console.log("My favourite number is: " + favoriteNumber)
