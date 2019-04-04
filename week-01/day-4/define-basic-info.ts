4a3bd3fed517f0643b0f6e8f455c75d8e9debd18
szabolcsveer <futuremove@gmail.com>
2019-04-04 16:17:08 +0200

    print msg's added

--- week-01/day-4/define-basic-info.ts
+++ week-01/day-4/define-basic-info.ts
@@ -10,5 +10,10 @@
 
 let myNameIs: string = "Szabika";
 let myAgeIs: number = 32;
-let myHeightIs: any = 1.78m;
+let myHeightIs: any = "1.78m";
 let maritalStatus: boolean = false;
+
+console.log(myNameIs);
+console.log(myAgeIs);
+console.log(myHeightIs);
+console.log(maritalStatus);
