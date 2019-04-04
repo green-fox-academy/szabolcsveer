66f7887df7e12cc5d048a4e8759626332c213378
szabolcsveer <futuremove@gmail.com>
2019-04-04 22:03:43 +0200

    for loop created, task solved

---
+++ week-01/day-4/iwontcheatontheexams/i-wont-cheat-on-the-exam.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+for (let index = 0; index < 101; index++) {
+    console.log( index + " I won't cheat on the exam!")
+    
+}
