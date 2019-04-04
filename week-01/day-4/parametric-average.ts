4d564b19f920833e7cdbb8f805377dc658a08cee
szabolcsveer <futuremove@gmail.com>
2019-04-04 23:14:09 +0200

    feat(added for loop for Sum)

---
+++ week-01/day-4/parametric-average.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Write a program that calculates the sum and the average from 1 to a given number.
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+
+let input: number = 5;
+let Sum: number = 0;
+for (var index = 1; index <input; index++) {
+    Sum += index
+    
+}
+console.log("Sum: " +Sum);
+console.log("Average: " + (Sum/index));
+console.log(index)
