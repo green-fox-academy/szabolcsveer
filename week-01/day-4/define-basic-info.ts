7323c81aa875d07f06e167cd7dde1cadd2354650
szabolcsveer <futuremove@gmail.com>
2019-04-04 16:08:26 +0200

    declared variables

---
+++ week-01/day-4/define-basic-info.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let chocolateBar: string = "Tasty!";
+let dailySpending: number = 2500;
+let theSkyIsBlue: any = true;
+
