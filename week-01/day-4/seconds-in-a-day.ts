2ca529bbfe90e78b1ccaa028cec2e6bea225499f
szabolcsveer <futuremove@gmail.com>
2019-04-04 17:07:17 +0200

    feat(Calc for seconds remaining in the given day added

--- week-01/day-4/seconds-in-a-day.ts
+++ week-01/day-4/seconds-in-a-day.ts
@@ -9,3 +9,10 @@ let currentSeconds: number = 42;
 
 let secondsIn24Hours: number = (24*60)*60;
 console.log(secondsIn24Hours)
+
+let currHoursInSECONDS: number = (14*60)*60;
+let currMinsInSECONDS: number = 34*60;
+
+let secondsRemaining: number = secondsIn24Hours - (currHoursInSECONDS+currMinsInSECONDS+currentSeconds);
+
+console.log("Seconds remaining from the given day: " + secondsRemaining);
