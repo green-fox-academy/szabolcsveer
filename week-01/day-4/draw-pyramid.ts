f51551e5c3988e31ae3ab7ff927d82d6bab6fbf8
szabolcsveer <futuremove@gmail.com>
2019-04-05 10:56:58 +0200

    draw-pyramid solved

---
+++ week-01/day-4/draw-pyramid.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+var lineCount: number = 44;
+
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
+
+
+
+for (var index = 0; index < lineCount; index++) {
+    var empty: any = "";
+    for (var i = 1; i < lineCount-index; i++){
+        empty = empty + " ";
+    }
+    for (var s = 1; s <= (2*index+1); s++) {
+        empty = empty + '*';
+      }
+    
+    console.log(empty);
+}
+
+
