e4f15a13b399cd35e755b92f18b6cebf70fd1b1c
szabolcsveer <futuremove@gmail.com>
2019-04-08 16:40:10 +0200

    append-a to items in array done

---
+++ week-02/day-01/append-a.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+
+let animals: string[] = ["koal", "pand", "zebr"];
+
+animals.forEach(function(item,index){
+    console.log(item + "a");
+    
+});
