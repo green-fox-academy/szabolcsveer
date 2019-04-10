683c5f7688dc6ecc8c310ad263fc02e996596486
szabolcsveer <futuremove@gmail.com>
2019-04-10 09:37:43 +0200

    feat:(vertical and horizontal line add to canvas)

---
+++ week-02/day-03/MiddleLines/middlelines.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// draw a red horizontal line to the canvas' middle.
+// draw a green vertical line to the canvas' middle.
+
+canvas.width = canvas.scrollWidth;
+canvas.height = canvas.scrollHeight;
+
+
+
+ctx.beginPath();
+ctx.moveTo(0, 200);
+ctx.lineTo(600, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(300, 0);
+ctx.lineTo(300,400);
+ctx.stroke();
