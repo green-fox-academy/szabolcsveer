e61e7ff2541d62784ddec7851f6a4f4efa362526
szabolcsveer <futuremove@gmail.com>
2019-04-11 10:35:44 +0200

    feat(numberadder.ts) add numberadder function using recursion

--- week-02/day-04/NumberAdder/numberadder.ts
+++ week-02/day-04/NumberAdder/numberadder.ts
@@ -1,5 +1,14 @@
-'use strict':
+'use strict';
 
 // Number adder
 // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+function numberAdder(n: number): number{
+    if (n <= 1){
+        return 1;
+    }
+    return n + numberAdder(n - 1);
+}
+
+console.log(numberAdder(10));
 
