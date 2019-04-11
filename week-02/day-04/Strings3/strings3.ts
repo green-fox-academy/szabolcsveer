b4b85ca0f8cd0b2693cb8b73a7ba2290c722a871
szabolcsveer <futuremove@gmail.com>
2019-04-11 17:35:32 +0200

    chore(strings3.ts) needs more work

---
+++ week-02/day-04/Strings3/strings3.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+// Strings again and again
+
+// Given a string, 
+// compute recursively a new string where all the adjacent chars are now separated by a *
+
+function addStars(input:string):string{
+    if(input.length === 0){
+    return input
+    }
+    return 
+    
+}
+
+console.log(addStars('pulsar'))
+
+
+
+
+
+
+
+
+
+export{}
