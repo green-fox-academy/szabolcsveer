3353d1def533bddad254ce31b4b003891a851354
szabolcsveer <futuremove@gmail.com>
2019-04-11 12:33:13 +0200

    feat(greatestcommondiv.ts) add divisor function

---
+++ week-02/day-04/GreatestCommonDivisor/greatestcommondiv.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+// Greatest Common Divisor
+// Find the greatest common divisor of two numbers using recursion.
+
+function divisor(n,z){
+    if(z === 0){
+        return n;
+    }
+    return divisor(z, n % z);
+
+}
+
+console.log(divisor(15,4));
