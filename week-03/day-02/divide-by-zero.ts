fbac53e7b4cae53a2e881cea6145d72a70e78230
szabolcsveer <futuremove@gmail.com>
2019-04-16 11:09:29 +0200

    chore(dividebyzero.ts) function needs work

---
+++ week-03/day-02/divide-by-zero.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+function divider(n){
+  let result = n/10;
+}
+
+try {
+  divider(0)
+} catch (error) {
+    if 
+}
