24a2f288a0c591311d0dc130046e077b2a898413
szabolcsveer <futuremove@gmail.com>
2019-04-17 13:42:51 +0200

    feat sharpie class add

---
+++ week-03/day-03/sharpie.ts
@@ -0,0 +1,26 @@
+'use strict';
+export{}
+
+class Sharpie {
+  color: string;
+  width: number;
+  inkAmount: number;
+
+  constructor(color: string,width:number,inkAmount:number = 100.0) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = inkAmount;
+    
+  }
+
+  use(){
+    this.inkAmount -= 0.25;
+    console.log(this.inkAmount)
+  }
+
+
+}
+
+const toll1: Sharpie = new Sharpie('blue',0.8)
+
+toll1.use();
