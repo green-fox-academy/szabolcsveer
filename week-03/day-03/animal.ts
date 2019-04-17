074a47705edb7a2701d0908f80d4e166f46e527b
szabolcsveer <futuremove@gmail.com>
2019-04-17 12:35:23 +0200

    add animal class with features

---
+++ week-03/day-03/animal.ts
@@ -0,0 +1,34 @@
+'use strict';
+export{}
+
+class Animal{
+  hunger: number;
+  thirst: number;
+
+  constructor(hunger: number = 50,thirst: number = 50){
+    this.hunger = hunger;
+    this.thirst = thirst;
+
+  }
+  eat(){
+    this.hunger -= 1;
+    console.log(`If this animal eats, its hunger is: ${this.hunger}`);
+    
+  }
+  drink(){
+    this.thirst -= 1;
+    console.log(`If this animal eats, its hunger is: ${this.thirst}`);
+  }
+  play(){
+    this.hunger += 1;
+    this.thirst += 1;
+    console.log(`If this animal eats, its hunger and thirst are: ${this.hunger} + ${this.thirst}`);
+
+  }
+
+  
+}
+
+const Zebra: Animal = new Animal();
+Zebra.eat();
+Zebra.play();
