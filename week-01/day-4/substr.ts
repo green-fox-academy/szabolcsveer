dd861eed9a383cb1ae9584014caf3d629773d771
szabolcsveer <futuremove@gmail.com>
2019-04-21 14:32:42 +0200

    substring solution add

---
+++ week-01/day-4/substr.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+//  Create a function that takes two strings as a parameter
+//  Returns the starting index where the second one is starting in the first one
+//  Returns `-1` if the second string is not in the first one
+
+function substr(str: string, keyword: string) : void {
+  if(str.includes(keyword)){
+    console.log(str.indexOf(keyword));
+  }else{
+   console.log('-1');
+   
+  }
+  
+  
+}
+
+//  Example
+
+// should print: `17`
+console.log(substr("this is what I'm searching in", "searching"));
+
+// should print: `-1`
+console.log(substr("this is what I'm searching in", "ooop"));
