cfa3b135ea01b0f8e860aff07c785a8354270da2
szabolcsveer <futuremove@gmail.com>
2019-05-06 16:30:14 +0200

    feat(doubled.ts) function add, result incorrect

---
+++ week-03/day-02/Doubled/doubled.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+// Create a method that decrypts duplicated-chars.txt
+
+const fs = require('fs');
+let duplicatedText = fs.readFileSync('duplicated-chars.txt','utf-8').split('\n')
+
+function decryptDoubles(duplicatedText){
+  let decrypted = '';
+  for(let i = 0 ; i < duplicatedText.length; i++){
+    for(let j = 0 ; j < duplicatedText[i].length; j+=2){
+    decrypted += duplicatedText[i][j]
+    decrypted += '\n' 
+  }
+
+return decrypted
+}
+}
+console.log(decryptDoubles(duplicatedText))
+
+
+
+
+
+
+
+
+
+
+
+
+export{}
