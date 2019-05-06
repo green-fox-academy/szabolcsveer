db26f821a269e007fbb8caa443b39b0dfe5fd6d9
szabolcsveer <futuremove@gmail.com>
2019-05-06 13:01:43 +0200

    feat(logs.ts) return IP and GETPOST ratio func add

--- week-03/day-02/logs.ts
+++ week-03/day-02/logs.ts
@@ -1,4 +1,7 @@
 'use strict';
+
+import { log } from "util";
+
 export{}
 
 // Read all data from 'log.txt'.
@@ -10,11 +13,34 @@ let fs = require('fs');
 // fs.readLineSync('log.txt')
 
 function returnIP(file){
-  let new1 = fs.readLineSync(file);
-  let newArr = new1.split(' ')
-  console.log(newArr);  
+  let new1 = fs.readFileSync(file,'utf-8');
+  let splitByLines = new1.split('\n')
+  let ip: any[] = []
+  let arr: any[] = []
+  arr.forEach(element => { element.replace(' /', '')}); 
+  
+  console.log(arr)
+    let postAndGet: string [] = []
+    for (let i: number = 0; i < splitByLines.length; i++) {
+        
+        arr.push(splitByLines[i].split('   '))
+        
+        ip.push(arr[i][1])
+        postAndGet.push(arr[i][2])
+      }
+      
+      const uniqueIps = ip.filter((v, i, a) => a.indexOf(v) === i); 
+      
+      const getNum = postAndGet.filter(x => x === 'GET').length 
+      const postNum = postAndGet.filter(x => x === 'POST').length;
+      
+      console.log(getNum,postNum);
+      
+    
 }
 
 
+
+returnIP('log.txt')
 
 // console.log(returnIP('log.txt'));
