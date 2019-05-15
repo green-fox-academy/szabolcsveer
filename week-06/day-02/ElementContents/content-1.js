0ccdb4f909183f1199b78d1076c5d50211e1f256
szabolcsveer <futuremove@gmail.com>
2019-05-15 13:20:30 +0200

    feat innerHTML changes contents in every element

---
+++ week-06/day-02/ElementContents/content-1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+const apple = document.querySelector('.apple');
+const ballon = document.querySelector('.balloon');
+const cat = document.querySelector('.cat');
+const animals = document.querySelector('.animals');
+
+
+apple.innerHTML = animals.innerHTML;
+cat.innerHTML = animals.innerHTML;
+ballon.innerHTML = animals.innerHTML; 
+
